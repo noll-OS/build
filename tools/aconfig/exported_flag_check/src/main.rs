756a2f817c78b0c7753e47a64c77e4d256943562
zhidou <zhidou@google.com>
2025-03-28 16:25:38 +0000

    add filter command in exported_flag_check
    This change add filter-api-flags command to this tool. It should be
    triggered when the build system creates the exported flags jar of all
    flags for API. It will remove all flags exported but not for APIs from
    the final jar.
    
    Test: atest exported-flag-check-test
    Bug: 345798636
    Change-Id: I3af0b915a6f3e6ba07d41a5f24ecac7f7f8f37a6

--- tools/aconfig/exported_flag_check/src/main.rs
+++ tools/aconfig/exported_flag_check/src/main.rs
@@ -15,18 +15,19 @@
  */
 
 //! `exported-flag-check` is a tool to ensures that exported flags are used as intended
-use anyhow::{ensure, Result};
-use clap::Parser;
-use std::{collections::HashSet, fs::File, path::PathBuf};
+use anyhow::{anyhow, bail, ensure, Context, Result};
+use clap::{builder::ArgAction, Arg, ArgMatches, Command};
+use std::io::Write;
+use std::{collections::HashSet, fs, fs::File, io::Read, path::PathBuf};
 
 mod utils;
 
 use utils::{
-    check_all_exported_flags, extract_flagged_api_flags, get_exported_flags_from_binary_proto,
-    read_finalized_flags,
+    check_all_exported_flags, extract_flagged_api_flags, filter_api_flags,
+    get_exported_flags_from_binary_proto, read_flag_from_binary, FlagId,
 };
 
-const ABOUT: &str = "CCheck Exported Flags
+const HELP: &str = "CCheck Exported Flags
 
 This tool ensures that exported flags are used as intended. Exported flags, marked with
 `is_exported: true` in their declaration, are designed to control access to specific API
@@ -44,49 +45,120 @@ This tool works as follows:
   - Error if exported flags are not in the set
 ";
 
-#[derive(Parser, Debug)]
-#[clap(about=ABOUT)]
-struct Cli {
-    #[arg(long)]
-    parsed_flags_file: PathBuf,
-
-    #[arg(long)]
-    api_signature_file: Vec<PathBuf>,
-
-    #[arg(long)]
-    finalized_flags_file: PathBuf,
+fn cli() -> Command {
+    Command::new("exported-flag-check")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("validate-exported-flags")
+                .arg(Arg::new("parsed-flags-file").long("parsed-flags-file").required(true))
+                .arg(
+                    Arg::new("api-signature-file")
+                        .long("api-signature-file")
+                        .required(true)
+                        .action(ArgAction::Append),
+                )
+                .arg(Arg::new("finalized-flags-file").long("finalized-flags-file").required(true)),
+        )
+        .subcommand(
+            Command::new("filter-api-flags")
+                .arg(Arg::new("cache").long("cache").required(true))
+                .arg(Arg::new("out").long("out").required(true)),
+        )
+        .after_help(HELP.trim())
 }
 
-fn main() -> Result<()> {
-    let args = Cli::parse();
+fn open_single_file(matches: &ArgMatches, arg_name: &str) -> Result<Box<dyn Read>> {
+    let Some(path) = matches.get_one::<String>(arg_name) else {
+        bail!("missing argument {}", arg_name);
+    };
+    Ok(Box::new(File::open(path)?))
+}
 
+fn open_multiple_files(matches: &ArgMatches, arg_name: &str) -> Result<Vec<Box<dyn Read>>> {
+    let mut opened_files: Vec<Box<dyn Read>> = Vec::new();
+    for path in matches.get_many::<String>(arg_name).unwrap_or_default() {
+        opened_files.push(Box::new(File::open(path)?));
+    }
+    Ok(opened_files)
+}
+
+fn validate_exported_flags<R: Read>(
+    parsed_flags_file: R,
+    api_signature_files: Vec<R>,
+    finalized_flags_file: R,
+    allow_flag_file: R,
+    allow_flag_package: R,
+) -> Result<Vec<FlagId>> {
     let mut flags_used_with_flaggedapi_annotation = HashSet::new();
-    for path in &args.api_signature_file {
-        let file = File::open(path)?;
+    for file in api_signature_files {
         let flags = extract_flagged_api_flags(file)?;
         flags_used_with_flaggedapi_annotation.extend(flags);
     }
-
-    let file = File::open(args.parsed_flags_file)?;
-    let all_flags = get_exported_flags_from_binary_proto(file)?;
-
-    let file = File::open(args.finalized_flags_file)?;
-    let already_finalized_flags = read_finalized_flags(file)?;
+    let all_flags = get_exported_flags_from_binary_proto(parsed_flags_file)?;
+    let already_finalized_flags = read_flag_from_binary(finalized_flags_file)?;
+    let allow_flag_set = read_flag_from_binary(allow_flag_file)?;
+    let allow_package_set = read_flag_from_binary(allow_flag_package)?;
 
     let exported_flags = check_all_exported_flags(
         &flags_used_with_flaggedapi_annotation,
         &all_flags,
         &already_finalized_flags,
+        &allow_flag_set,
+        &allow_package_set,
     )?;
 
     println!("{}", exported_flags.join("\n"));
 
-    ensure!(
-        exported_flags.is_empty(),
-        "Flags {} are exported but not used to guard any API. \
-    Exported flag should be used to guard API",
-        exported_flags.join(",")
-    );
+    Ok(exported_flags)
+}
+
+fn main() -> Result<()> {
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("validate-exported-flags", sub_matches)) => {
+            let parsed_flags_file = open_single_file(sub_matches, "parsed-flags-file")?;
+            let api_signature_files = open_multiple_files(sub_matches, "api-signature-file")?;
+            let finalized_flags_file = open_single_file(sub_matches, "finalized-flags-file")?;
+            let allow_flag_file = include_str!("../allow_flag_list.txt");
+            let allow_flag_package = include_str!("../allow_package_list.txt");
+
+            let exported_flags = validate_exported_flags(
+                parsed_flags_file,
+                api_signature_files,
+                finalized_flags_file,
+                Box::new(allow_flag_file.as_bytes()),
+                Box::new(allow_flag_package.as_bytes()),
+            )?;
+
+            ensure!(
+                exported_flags.is_empty(),
+                "Flags {} are exported but not used to guard any API. \
+            Exported flag should be used to guard API",
+                exported_flags.join(",")
+            );
+        }
+        Some(("filter-api-flags", sub_matches)) => {
+            let cache = open_single_file(sub_matches, "cache")?;
+
+            let Some(out_file_arg) = sub_matches.get_one::<String>("out") else {
+                bail!("argument out is missing");
+            };
+            let out_file = PathBuf::from(out_file_arg);
+            let allow_flag_file = &include_str!("../allow_flag_list.txt");
+            let filtered_cache = filter_api_flags(cache, Box::new(allow_flag_file.as_bytes()))?;
+            let parent = out_file
+                .parent()
+                .ok_or(anyhow!("unable to locate parent of output file {}", out_file.display()))?;
+            fs::create_dir_all(parent)
+                .with_context(|| format!("failed to create directory {}", parent.display()))?;
+            let mut file = fs::File::create(&out_file)
+                .with_context(|| format!("failed to open {}", out_file.display()))?;
+            file.write_all(&filtered_cache)
+                .with_context(|| format!("failed to write to {}", out_file.display()))?;
+        }
+        _ => unreachable!(),
+    }
+
     Ok(())
 }
 
@@ -96,22 +168,23 @@ mod tests {
 
     #[test]
     fn test() {
-        let input = include_bytes!("../tests/api-signature-file.txt");
-        let flags_used_with_flaggedapi_annotation = extract_flagged_api_flags(&input[..]).unwrap();
+        let flags_used_with_flaggedapi_annotation =
+            vec![&include_bytes!("../tests/api-signature-file.txt")[..]];
+        let all_flags_to_be_finalized = include_bytes!("../tests/flags.protobuf");
+        let already_finalized_flags = include_bytes!("../tests/finalized-flags.txt");
+        let allow_flag_file = "record_finalized_flags.test.boo".as_bytes();
+        let allow_flag_package = "".as_bytes();
 
-        let input = include_bytes!("../tests/flags.protobuf");
-        let all_flags_to_be_finalized = get_exported_flags_from_binary_proto(&input[..]).unwrap();
-
-        let input = include_bytes!("../tests/finalized-flags.txt");
-        let already_finalized_flags = read_finalized_flags(&input[..]).unwrap();
-
-        let exported_flags = check_all_exported_flags(
-            &flags_used_with_flaggedapi_annotation,
-            &all_flags_to_be_finalized,
-            &already_finalized_flags,
+        let exported_flags = validate_exported_flags(
+            &all_flags_to_be_finalized[..],
+            flags_used_with_flaggedapi_annotation,
+            &already_finalized_flags[..],
+            allow_flag_file,
+            allow_flag_package,
         )
         .unwrap();
 
         assert_eq!(1, exported_flags.len());
+        assert_eq!("record_finalized_flags.test.not_enabled", exported_flags[0]);
     }
 }
