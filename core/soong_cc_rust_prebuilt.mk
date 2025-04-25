f051a86ce58740bdd9372ad150861fac2576ae51
Cole Faust <colefaust@google.com>
2025-04-25 10:49:41 -0700

    Remove gcov-report-files-all.zip
    This artifact exists on some CI builds, like
    aosp_cf_x86_64_only_phone-trunk_staging-userdebug_coverage,
    aosp_cf_x86_64_phone-trunk_staging-userdebug_coverage_bionic, and
    cf_x86_64_phone-trunk_staging-userdebug_coverage, but it's always
    an empty zip. I checked with qingshen@ and she confirmed it's deprecated
    and unused. Remove it.
    
    Also remove all usages of the *_OUT_COVERAGE variables as they were
    only used for building this zip.
    
    Bug: 413425319
    Test: Presubmits
    Change-Id: I0f9a5ab219720c250bbf81323e29451f3aa88bd1

--- core/soong_cc_rust_prebuilt.mk
+++ core/soong_cc_rust_prebuilt.mk
@@ -203,16 +203,6 @@ endif
 ifeq ($(NATIVE_COVERAGE),true)
   ifneq (,$(strip $(LOCAL_PREBUILT_COVERAGE_ARCHIVE)))
     $(eval $(call copy-one-file,$(LOCAL_PREBUILT_COVERAGE_ARCHIVE),$(intermediates)/$(LOCAL_MODULE).zip))
-    ifneq ($(LOCAL_UNINSTALLABLE_MODULE),true)
-      ifdef LOCAL_IS_HOST_MODULE
-        my_coverage_path := $($(my_prefix)OUT_COVERAGE)/$(patsubst $($(my_prefix)OUT)/%,%,$(my_module_path))
-      else
-        my_coverage_path := $(TARGET_OUT_COVERAGE)/$(patsubst $(PRODUCT_OUT)/%,%,$(my_module_path))
-      endif
-      my_coverage_path := $(my_coverage_path)/$(patsubst %.so,%,$(my_installed_module_stem)).zip
-      $(eval $(call copy-one-file,$(LOCAL_PREBUILT_COVERAGE_ARCHIVE),$(my_coverage_path)))
-      $(LOCAL_BUILT_MODULE): $(my_coverage_path)
-    endif
   else
     # Coverage information is needed when static lib is a dependency of another
     # coverage-enabled module.
