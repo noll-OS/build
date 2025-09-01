5fcd93246803452d325028abba14573cb66e32b6
Subhash Reddy Surkanti (xWF) <subhashreddy@google.com>
2025-09-01 02:16:01 -0700

    Revert "Preinstall VirtualDeviceManager on media_system products."
    This reverts commit 5e1c28f23d4015b5c6c6182a2038819527056dab.
    
    Reason for revert: Likely culprit for b/442336184  - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Bug: 442336184
    Change-Id: I481cb26119302844b69deeaddb56c74da48861db

--- target/product/media_system.mk
+++ target/product/media_system.mk
@@ -38,7 +38,6 @@ PRODUCT_PACKAGES += \
 
 ifeq ($(RELEASE_PACKAGE_COMPUTER_CONTROL),true)
   PRODUCT_PACKAGES += VirtualDeviceManager
-  PRODUCT_PACKAGES += preinstalled-packages-media-system.xml
 endif
 
 PRODUCT_HOST_PACKAGES += \
